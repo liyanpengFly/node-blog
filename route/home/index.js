d2451ede04c622f93afb620a51b4ab7b6c6f4c68
liyanpeng <1628586422@qq.com>
2021-05-07 22:09:59 +0800

    博客首页面数据渲染-数据库查询-author联合查询-mongoose-sex-page

--- route/home/index.js
+++ route/home/index.js
@@ -1,3 +1,8 @@
-module.exports = (req,res)=>{
-    res.render('home/default.art')
+const { Article } = require('../../model/article')
+const pagination = require('mongoose-sex-page')
+module.exports = async (req,res)=>{
+    const result = await pagination(Article).page(1).size(4).display(2).find().populate('author').exec()
+    var newResult = JSON.stringify(result)
+    newResult = JSON.parse(newResult)
+    res.render('home/default.art',{ result:newResult })
 }
