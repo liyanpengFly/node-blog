de0de1105672218a8a4f366b0e104e075c6c9a23
liyanpeng <1628586422@qq.com>
2021-05-09 15:44:21 +0800

    文章详情页面数据展示

--- route/home/article.js
+++ route/home/article.js
@@ -1,3 +1,13 @@
-module.exports = (req,res)=>{
-    res.render('home/article.art')
+// 导入文章构造集合
+const { Article } = require('../../model/article')
+module.exports = async (req,res)=>{
+    // 接受客户端传递过来的文章id值
+    let { id }  = req.query
+    // 根据id查询文章详情
+    var  article = await Article.findOne({_id:id}).populate('author')
+    // res.send(article)
+    // 将数据传入模板
+    article = JSON.stringify(article)
+    article = JSON.parse(article)
+    res.render('home/article.art', { article })
 }
