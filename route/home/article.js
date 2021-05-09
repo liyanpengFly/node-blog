10b31fdb7a500daa6b65f30c2a9568715346590a
liyanpeng <1628586422@qq.com>
2021-05-09 20:28:10 +0800

    实现评论展示功能

--- route/home/article.js
+++ route/home/article.js
@@ -1,13 +1,20 @@
 // 导入文章构造集合
 const { Article } = require('../../model/article')
+const { Comment } = require('../../model/comment')
 module.exports = async (req,res)=>{
     // 接受客户端传递过来的文章id值
     let { id }  = req.query
     // 根据id查询文章详情
     var  article = await Article.findOne({_id:id}).populate('author')
-    // res.send(article)
+
+    var comment = await Comment.find({aid:id}).populate('uid')
+    // res.send(comment)
+    // return 
     // 将数据传入模板
     article = JSON.stringify(article)
     article = JSON.parse(article)
-    res.render('home/article.art', { article })
+
+    comment = JSON.stringify(comment)
+    comment = JSON.parse(comment)
+    res.render('home/article.art', { article,comment })
 }
